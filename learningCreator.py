efff514f001dce84e5bdf3e65ea58092b29bea1a
Robosword <robotanmol@gmail.com>
2026-08-31 12:42:59 -0700

    Make a nn.Sequential so that the neutral network fits in one line, for input layer use conv2d to extract data from it, and rearrange it.

--- learningCreator.py
+++ learningCreator.py
@@ -6,4 +6,10 @@ import torch.nn.functional as F
 
 class PatchEmbeddings(nn.Module):
     def __init__(self, in_channels: int = 3, patch_size: int=16, emb_size:int = 768, img_size-224):
-        pass
+        self.patch_size = patch_size
+        super().__init__()
+        self.embed = nn.Sequential(
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) -> b (h w) e')
+        )
+        
