b726ed2bd62e29014df17acd9aa94b8cca2992fa
Robosword <robotanmol@gmail.com>
2026-08-31 12:37:28 -0700

    Using Patch Embeddings to take the image and turn it from a 2d array of pixels to a 1d array while saving the positions.

--- learningCreator.py
+++ learningCreator.py
@@ -3,3 +3,7 @@ import torch.nn as nn
 import torch
 import pathlib
 import torch.nn.functional as F
+
+class PatchEmbeddings(nn.Module):
+    def __init__(self, in_channels: int = 3, patch_size: int=16, emb_size:int = 768, img_size-224):
+        pass
