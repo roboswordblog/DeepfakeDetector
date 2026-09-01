af005a023843c9da6ebed8dab6b0756907d13d42
Robosword <robotanmol@gmail.com>
2026-09-01 10:20:59 -0700

    Make all these tuff to instantiate the stuff I need to instantiate the transformer and load it

---
+++ modelLoader.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+import torch
+import pathlib
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange
+from einops import rearrange
+
+def PositionalEmbedding(seq_len, emb_size):
+    embeddings = torch.ones(seq_len, emb_size)
+    for i in range(seq_lean):
+        for j in range(emb_size):
+            embeddings[i][j] = np.sin(i / (pow(10000, j/emb_size)) if j % 2 == 0 else np.cos(i / pow(10000, (j-1) / emb_size)))
+    return torch.tensor(embeddings)
+
+class PatchEmbeddings(nn.Module):
+    def __init__(self, in_channels: int = 3, patch_size: int=16, emb_size:int = 768, img_size=224):
+        self.patch_size = patch_size
+        super().__init__()
+        self.embed = nn.Sequential(
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) -> b (h w) e')
+        )
+        self.cls_token = nn.Parameter(torch.rand(1, 1, emb_size))
+        self.pos_embed = nn.Parameter(PositionalEmbedding((img_size // patch_size)**2 + 1, emb_size))
+    
+    def  forward(self, x:Tensor) -> Tensor:
+        b, _, _, _ = x.shape
+        x = self.embed(x)
+        return x
+
+class MultiHead(nn.Module):
+    def __init__(self, emb_size, num_head):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_head = num_head
+        self.key = nn.Linear(emb_size, emb_size)
+        self.value = nn.Linear(emb_size, emb_size)
+        self.query =  nn.Linear(emb_size, emb_size)
+        self.att_dr = nn.Dropout(0.1)
+
+    def forward(self, x):
+        k = rearrange(self.key(x), 'b n (h e) - > b h n e', h=self.num_head)
+        q = rearrange(self.query(x), 'b n (h e) - > b h n e', h=self.num_head)       
+        v = rearrange(self.value(x), 'b n (h e) - > b h n e', h=self.num_head)
+        wei = q@k.transpose(3,2)/self.head_dim ** 0.5
+        wei = F.softmax(wei, dim=2)
+        wei = self.att_dr(wei)
+        out = wei@v
+        out = rearrange(out, 'b h n e -> b n (h e)')
+        return out
+      
+class FeedForward(nn.Module):
+    def __init__(self, emb_size):
+        super().__init__()
+        self.ff = nn.Sequential(
+            nn.Linear(emb_size, 4*emb_size),
+            nn.Linear(emb_size, 4*emb_size)
+        )
+
+    def forward(self, x):
+        return self.ff(x)
+
+class Block(nn.Module):
+    def __init__(self, emb_size, num_head):
+        super().__init__()
+        self.att = MultiHead(emb_size, num_head)
+        self.ll = nn.LayerNorm(emb_size)
+        self.dropout = nn.Dropout(0.1)
+        self.ff = FeedForward(emb_size)
+
+    def forward(self, x):
+        x = x + self.dropout(self.att(self.ll(x)))
+        x = x +  self.dropout(self.ff(self.ll(x)))
+        return x
+
+class VisionTransformer(nn.Module):
+    def __init__(self, num_layers, img_size, emb_size, patch_size, num_head, num_class):
+        super().__init__()
+        self.attention = nn.Sequential(*[Block (emb_size, num_head) for _ in range(num_layers)])
+        self.patchemb = PatchEmbedding(patch_size=patch_size, img_size=img_size)
+        self.ff = nn.Linear(emb_size, num_class)
+
