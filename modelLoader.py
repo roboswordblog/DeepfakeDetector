c0f627dbbb42d1682973a0f0af78484655db1b72
Robosword <robotanmol@gmail.com>
2026-09-01 10:35:41 -0700

    load the model

--- modelLoader.py
+++ modelLoader.py
@@ -79,4 +79,20 @@ class VisionTransformer(nn.Module):
         self.attention = nn.Sequential(*[Block (emb_size, num_head) for _ in range(num_layers)])
         self.patchemb = PatchEmbedding(patch_size=patch_size, img_size=img_size)
         self.ff = nn.Linear(emb_size, num_class)
+        
+num_layers = 8
+emb_size = 768
+num_head = 6
+num_class = 10
+patch_size = 16
+model = VisionTransformer( num_layers = num_layers,
+                            img_size = 224,
+                            emb_size = emb_size
+                            num_head= num_head,
+                            patch_size = patch_size,
+                            num_class = num_class).to(device)
+
+load_path = pathlib.Path("model_weights.pth")
+loaded_model.load_state_dict(torch.load(load_path, weights_only=True))
+loaded_model.eval()
 
