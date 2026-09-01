decacace9e55f1bc2adb2ec05327cc9d497d699d
Robosword <robotanmol@gmail.com>
2026-09-01 08:19:48 -0700

    Add patch embedding and the ff network.

--- learningCreator.py
+++ learningCreator.py
@@ -77,5 +77,7 @@ class VisionTransformer(nn.Module):
     def __init__(self, num_layerss, img_size, emb_size, patch_size, num_head, num_class):
         super().__init__()
         self.attention = nn.Sequential(*[Block (emb_size, num_head) for _ in range(num_layers)])
+        self.patchemb = PatchEmbedding(patch_size=patch_size, img_size=img_size)
+        self.ff = nn.Linear(emb_size, num_class)
         
         
